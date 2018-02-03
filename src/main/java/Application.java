4bb682f0dffb9fee72a9d1126eeb216f4ab16068
Danny Lekim <dannylekim@Dannys-MacBook-Pro.local>
2018-02-03 16:58:07 -0500

    begin userDAO

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -2,6 +2,7 @@ package main.java;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import main.java.Configuration;
 
 @SpringBootApplication
 public class Application {
